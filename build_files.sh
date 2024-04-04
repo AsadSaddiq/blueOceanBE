8ec6bc139c25266c132187449b3dd65c7446d4c6
AsadSaddiq <asadryk2911@gmail.com>
2024-04-04 21:59:08 +0500

    try to fix

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,4 @@ echo "BUILD START"
 /usr/bin/python3.9 -m pip --version
 echo python
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
