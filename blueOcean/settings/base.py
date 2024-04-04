6c1804a2cec02bd6943fa865d4eab9a6468f7b19
AsadSaddiq <asadryk2911@gmail.com>
2024-04-04 21:04:44 +0500

    try to fix

--- blueOcean/settings/base.py
+++ blueOcean/settings/base.py
@@ -107,8 +107,15 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'  # Corrected from MEDIA_URLS
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
