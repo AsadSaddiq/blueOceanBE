cb17cde06875561d7bc219a05e42e0abf317c874
AsadSaddiq <asadryk2911@gmail.com>
2024-04-04 21:15:54 +0500

    try to fix

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
- echo "BUILD START"
- /usr/bin/python3.9 -m ensurepip
- /usr/bin/python3.9 --version
- /usr/bin/python3.9 -m pip --version
- echo python
- python3.9 -m pip install -r requirements.txt
- python manage.py collectstatic --noinput --clear
- mkdir -p staticfiles_build
- cp -r static/* staticfiles_build/
- echo "BUILD END"
+echo "BUILD START"
+/usr/bin/python3.9 -m ensurepip
+/usr/bin/python3.9 --version
+/usr/bin/python3.9 -m pip --version
+echo python
+python3.9 -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+mkdir -p staticfiles_build
+cp -r static/* staticfiles_build/
+echo "BUILD END"
