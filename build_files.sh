b49199d85790b9aa07a5fe9cb97af5d6ded2db75
AsadSaddiq <asadryk2911@gmail.com>
2024-04-04 20:30:58 +0500

    try to fix

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,6 @@
  /usr/bin/python3.9 -m ensurepip
  /usr/bin/python3.9 --version
  /usr/bin/python3.9 -m pip --version
- source /path/to/your/venv/bin/activate
  echo python
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
