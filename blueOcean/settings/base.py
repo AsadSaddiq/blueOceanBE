defa0c010769259983ddbe07137c3f4824948c36
AsadSaddiq <asadryk2911@gmail.com>
2024-04-04 21:43:06 +0500

    try to fix

--- blueOcean/settings/base.py
+++ blueOcean/settings/base.py
@@ -109,7 +109,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 # MEDIA_URL = '/media/'  # Corrected from MEDIA_URLS
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
