abbd0834b0626066d9653a80d6d90cafa784fe93
AsadSaddiq <asadryk2911@gmail.com>
2024-04-04 08:33:35 +0500

    pip version

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
  echo "BUILD START"
+ /usr/bin/python3.9 -m ensurepip
+/usr/bin/python3.9 --version
+/usr/bin/python3.9 -m pip --version
  echo python
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
