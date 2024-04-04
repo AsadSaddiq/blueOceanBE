0b45b8baaa24459e17b8bc74c44411a40a313b91
AsadSaddiq <asadryk2911@gmail.com>
2024-04-04 20:29:05 +0500

    try to fix

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,6 @@
  /usr/bin/python3.9 -m ensurepip
  /usr/bin/python3.9 --version
  /usr/bin/python3.9 -m pip --version
- pip install psycopg2==2.9.5
- pip install psycopg2-binary==2.9.5
- pip install django
  source /path/to/your/venv/bin/activate
  echo python
  python3.9 -m pip install -r requirements.txt
