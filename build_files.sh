e77e0a327a49c23f22193ed43f303c6d7c60b59c
AsadSaddiq <asadryk2911@gmail.com>
2024-04-05 10:06:26 +0500

    try to fix

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@ echo "BUILD START"
 /usr/bin/python3.9 -m pip --version
 echo python
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo BASE_DIR
 echo "Files in deployment directory:"
